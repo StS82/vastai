29b6cba0a7121b54374c355e4e07eb2c9e71110f
StS82 <32516183+StS82@users.noreply.github.com>
2025-05-25 12:54:06 +0200

    Update provisioning.sh
    remove force pip install
    activate venv on service startup

--- provisioning.sh
+++ provisioning.sh
@@ -13,14 +13,14 @@ cd kohya_ss
 git checkout dev && git pull
 
 # Install your packages
-pip install -I xformers==0.0.30 && pip install -r requirements.txt && pip install -I bitsandbytes gradio tensorflow onnxruntime-gpu accelerate==0.30.0 "numpy<2"
+pip install xformers==0.0.30 && pip install -r requirements.txt && pip install bitsandbytes gradio tensorflow onnxruntime-gpu accelerate==0.30.0 "numpy<2"
 
 # Download some useful files
 wget -P "${WORKSPACE}/kohya_ss/models" https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/raw/main/sd_xl_base_1.0_0.9vae.safetensors
 
 # Set up any additional services
 cat > /etc/supervisor/conf.d/kohya_ss.conf<< EOF
-command=cd /workspace/kohya_ss && python3 /workspace/kohya_ss/kohya_gui.py --listen=0.0.0.0 --headless --noverify
+command=. /venv/main/bin/activate && cd /workspace/kohya_ss && python3 /workspace/kohya_ss/kohya_gui.py --listen=0.0.0.0 --headless --noverify
 autostart=true
 autorestart=true
 stderr_logfile=/var/log/kohya_ss.err.log
